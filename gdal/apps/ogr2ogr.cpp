aashish24/gdal-cmake

commit dd03f064aa6aa421ffbf67cfe69fa35ba982e253

Add better control over fields while copying features. (#3247)
git-svn-id: https://svn.osgeo.org/gdal/trunk@18226 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/apps/ogr2ogr.cpp
@@ -1150,7 +1150,13 @@ static int TranslateLayer( OGRDataSource *poSrcDS,
 /*      the selected fields, and in the order that they were            */
 /*      selected.                                                       */
 /* -------------------------------------------------------------------- */
-    int         iField;
+    int         iField = poFDefn->GetFieldCount(), *panMap;
+
+    // Initialize the index-to-index map to -1's
+    panMap = (int *) VSIMalloc( sizeof(int) * iField++ );
+    do{
+        panMap[--iField] = -1;
+    }while( iField );
 
     if (papszSelFields && !bAppend )
     {
@@ -1170,6 +1176,8 @@ static int TranslateLayer( OGRDataSource *poSrcDS,
                 }
                 else
                     poDstLayer->CreateField( poFDefn->GetFieldDefn(iSrcField) );
+
+                panMap[iSrcField] = iField;
             }
             else
             {
@@ -1195,6 +1203,8 @@ static int TranslateLayer( OGRDataSource *poSrcDS,
             }
             else
                 poDstLayer->CreateField( poFDefn->GetFieldDefn(iField) );
+
+            panMap[iField] = iField;
         }
     }
     
@@ -1238,7 +1248,7 @@ static int TranslateLayer( OGRDataSource *poSrcDS,
         CPLErrorReset();
         poDstFeature = OGRFeature::CreateFeature( poDstLayer->GetLayerDefn() );
 
-        if( poDstFeature->SetFrom( poFeature, TRUE ) != OGRERR_NONE )
+        if( poDstFeature->SetFrom( poFeature, panMap, TRUE ) != OGRERR_NONE )
         {
             if( nGroupTransactions )
                 poDstLayer->CommitTransaction();

gdal/ogr/ogrfeature.cpp
@@ -2703,6 +2703,102 @@ int OGR_F_Equal( OGRFeatureH hFeat, OGRFeatureH hOtherFeat )
 OGRErr OGRFeature::SetFrom( OGRFeature * poSrcFeature, int bForgiving )
 
 {
+/* -------------------------------------------------------------------- */
+/*      Retrieve the field ids by name.                                 */
+/* -------------------------------------------------------------------- */
+    int         iField, *panMap;
+
+    panMap = (int *) VSIMalloc( sizeof(int) * poSrcFeature->GetFieldCount() );
+    for( iField = 0; iField < poSrcFeature->GetFieldCount(); iField++ )
+    {
+        panMap[iField] = GetFieldIndex(
+            poSrcFeature->GetFieldDefnRef(iField)->GetNameRef() );
+
+        if( panMap[iField] == -1 )
+        {
+            if( bForgiving )
+                continue;
+            else
+                return OGRERR_FAILURE;
+        }
+    }
+
+    return SetFrom( poSrcFeature, panMap, bForgiving );
+}
+
+/************************************************************************/
+/*                           OGR_F_SetFrom()                            */
+/************************************************************************/
+
+/**
+ * \brief Set one feature from another.
+ *
+ * Overwrite the contents of this feature from the geometry and attributes
+ * of another.  The hOtherFeature does not need to have the same
+ * OGRFeatureDefn.  Field values are copied by corresponding field names.
+ * Field types do not have to exactly match.  OGR_F_SetField*() function 
+ * conversion rules will be applied as needed.
+ *
+ * This function is the same as the C++ method OGRFeature::SetFrom().
+ *
+ * @param hFeat handle to the feature to set to.
+ * @param hOtherFeat handle to the feature from which geometry,
+ * and field values will be copied.
+ *
+ * @param bForgiving TRUE if the operation should continue despite lacking
+ * output fields matching some of the source fields.
+ *
+ * @return OGRERR_NONE if the operation succeeds, even if some values are
+ * not transferred, otherwise an error code.
+ */
+
+OGRErr OGR_F_SetFrom( OGRFeatureH hFeat, OGRFeatureH hOtherFeat, 
+                      int bForgiving )
+
+{
+    VALIDATE_POINTER1( hFeat, "OGR_F_SetFrom", CE_Failure );
+    VALIDATE_POINTER1( hOtherFeat, "OGR_F_SetFrom", CE_Failure );
+
+    return ((OGRFeature *) hFeat)->SetFrom( (OGRFeature *) hOtherFeat, 
+                                           bForgiving );
+}
+
+/************************************************************************/
+/*                              SetFrom()                               */
+/************************************************************************/
+
+/**
+ * \brief Set one feature from another.
+ *
+ * Overwrite the contents of this feature from the geometry and attributes
+ * of another.  The poSrcFeature does not need to have the same
+ * OGRFeatureDefn.  Field values are copied according to the provided indices
+ * map. Field types do not have to exactly match.  SetField() method
+ * conversion rules will be applied as needed. This is more efficient than
+ * OGR_F_SetFrom() in that this doesn't lookup the fields by their names.
+ * Particularly useful when the field names don't match.
+ *
+ * This method is the same as the C function OGR_F_SetFromWithMap().
+ *
+ * @param poSrcFeature the feature from which geometry, and field values will
+ * be copied.
+ *
+ * @param panMap Array of the indices of the feature's fields
+ * stored at the corresponding index of the source feature's fields. A value of
+ * -1 should be used to ignore the source's field. The array should not be NULL
+ * and be as long as the number of fields in the source feature.
+ * 
+ * @param bForgiving TRUE if the operation should continue despite lacking
+ * output fields matching some of the source fields.
+ *
+ * @return OGRERR_NONE if the operation succeeds, even if some values are
+ * not transferred, otherwise an error code.
+ */
+
+OGRErr OGRFeature::SetFrom( OGRFeature * poSrcFeature, int *panMap ,
+                            int bForgiving )
+
+{
     OGRErr      eErr;
 
     SetFID( OGRNullFID );
@@ -2726,16 +2822,13 @@ OGRErr OGRFeature::SetFrom( OGRFeature * poSrcFeature, int bForgiving )
 
     for( iField = 0; iField < poSrcFeature->GetFieldCount(); iField++ )
     {
-        iDstField = GetFieldIndex(
-            poSrcFeature->GetFieldDefnRef(iField)->GetNameRef() );
+        iDstField = panMap[iField];
 
-        if( iDstField == -1 )
-        {
-            if( bForgiving )
-                continue;
-            else
-                return OGRERR_FAILURE;
-        }
+        if( iDstField < 0 )
+            continue;
+            
+        if( GetFieldCount() <= iDstField )
+            return OGRERR_FAILURE;
 
         if( !poSrcFeature->IsFieldSet(iField) )
         {
@@ -2794,7 +2887,7 @@ OGRErr OGRFeature::SetFrom( OGRFeature * poSrcFeature, int bForgiving )
 }
 
 /************************************************************************/
-/*                           OGR_F_SetFrom()                            */
+/*                      OGR_F_SetFromWithMap()                          */
 /************************************************************************/
 
 /**
@@ -2802,9 +2895,11 @@ OGRErr OGRFeature::SetFrom( OGRFeature * poSrcFeature, int bForgiving )
  *
  * Overwrite the contents of this feature from the geometry and attributes
  * of another.  The hOtherFeature does not need to have the same
- * OGRFeatureDefn.  Field values are copied by corresponding field names.
- * Field types do not have to exactly match.  OGR_F_SetField*() function 
- * conversion rules will be applied as needed.
+ * OGRFeatureDefn.  Field values are copied according to the provided indices
+ * map. Field types do not have to exactly match.  OGR_F_SetField*() function 
+ * conversion rules will be applied as needed. This is more efficient than
+ * OGR_F_SetFrom() in that this doesn't lookup the fields by their names.
+ * Particularly useful when the field names don't match.
  *
  * This function is the same as the C++ method OGRFeature::SetFrom().
  *
@@ -2812,6 +2907,11 @@ OGRErr OGRFeature::SetFrom( OGRFeature * poSrcFeature, int bForgiving )
  * @param hOtherFeat handle to the feature from which geometry,
  * and field values will be copied.
  *
+ * @param panMap Array of the indices of the destination feature's fields
+ * stored at the corresponding index of the source feature's fields. A value of
+ * -1 should be used to ignore the source's field. The array should not be NULL
+ * and be as long as the number of fields in the source feature.
+ * 
  * @param bForgiving TRUE if the operation should continue despite lacking
  * output fields matching some of the source fields.
  *
@@ -2819,15 +2919,16 @@ OGRErr OGRFeature::SetFrom( OGRFeature * poSrcFeature, int bForgiving )
  * not transferred, otherwise an error code.
  */
 
-OGRErr OGR_F_SetFrom( OGRFeatureH hFeat, OGRFeatureH hOtherFeat, 
-                      int bForgiving )
+OGRErr OGR_F_SetFromWithMap( OGRFeatureH hFeat, OGRFeatureH hOtherFeat, 
+                      int bForgiving, int *panMap )
 
 {
     VALIDATE_POINTER1( hFeat, "OGR_F_SetFrom", CE_Failure );
     VALIDATE_POINTER1( hOtherFeat, "OGR_F_SetFrom", CE_Failure );
+    VALIDATE_POINTER1( panMap, "OGR_F_SetFrom", CE_Failure);
 
     return ((OGRFeature *) hFeat)->SetFrom( (OGRFeature *) hOtherFeat, 
-                                           bForgiving );
+                                           panMap, bForgiving );
 }
 
 /************************************************************************/

gdal/ogr/ogr_api.h
@@ -281,6 +281,7 @@ long   CPL_DLL OGR_F_GetFID( OGRFeatureH );
 OGRErr CPL_DLL OGR_F_SetFID( OGRFeatureH, long );
 void   CPL_DLL OGR_F_DumpReadable( OGRFeatureH, FILE * );
 OGRErr CPL_DLL OGR_F_SetFrom( OGRFeatureH, OGRFeatureH, int );
+OGRErr CPL_DLL OGR_F_SetFromWithMap( OGRFeatureH, OGRFeatureH, int , int * );
 
 const char CPL_DLL *OGR_F_GetStyleString( OGRFeatureH );
 void   CPL_DLL OGR_F_SetStyleString( OGRFeatureH, const char * );

gdal/ogr/ogr_feature.h
@@ -271,6 +271,7 @@ class CPL_DLL OGRFeature
     void                DumpReadable( FILE *, char** papszOptions = NULL );
 
     OGRErr              SetFrom( OGRFeature *, int = TRUE);
+    OGRErr              SetFrom( OGRFeature *, int *, int = TRUE );
 
     OGRErr              RemapFields( OGRFeatureDefn *poNewDefn, 
                                      int *panRemapSource );

commit 50d768e38c2aceeb2e108973a39fc19697ad6e9c

Fix array overrun and memory leaks introduced by r18226 (#3247)
git-svn-id: https://svn.osgeo.org/gdal/trunk@18231 f0d54148-0727-0410-94bb-9a71ac55c965

gdal/apps/ogr2ogr.cpp
@@ -1150,13 +1150,13 @@ static int TranslateLayer( OGRDataSource *poSrcDS,
 /*      the selected fields, and in the order that they were            */
 /*      selected.                                                       */
 /* -------------------------------------------------------------------- */
-    int         iField = poFDefn->GetFieldCount(), *panMap;
+    int         nSrcFieldCount = poFDefn->GetFieldCount();
+    int         iField, *panMap;
 
     // Initialize the index-to-index map to -1's
-    panMap = (int *) VSIMalloc( sizeof(int) * iField++ );
-    do{
-        panMap[--iField] = -1;
-    }while( iField );
+    panMap = (int *) VSIMalloc( sizeof(int) * nSrcFieldCount );
+    for( iField=0; iField < nSrcFieldCount; iField++)
+        panMap[iField] = -1;
 
     if (papszSelFields && !bAppend )
     {
@@ -1184,7 +1184,10 @@ static int TranslateLayer( OGRDataSource *poSrcDS,
                 fprintf( stderr, "Field '%s' not found in source layer.\n", 
                         papszSelFields[iField] );
                 if( !bSkipFailures )
+                {
+                    VSIFree(panMap);
                     return FALSE;
+                }
             }
         }
     }
@@ -1259,6 +1262,7 @@ static int TranslateLayer( OGRDataSource *poSrcDS,
             
             OGRFeature::DestroyFeature( poFeature );
             OGRFeature::DestroyFeature( poDstFeature );
+            VSIFree(panMap);
             return FALSE;
         }
 
@@ -1298,6 +1302,7 @@ static int TranslateLayer( OGRDataSource *poSrcDS,
                     {
                         OGRFeature::DestroyFeature( poFeature );
                         OGRFeature::DestroyFeature( poDstFeature );
+                        VSIFree(panMap);
                         return FALSE;
                     }
                 }
@@ -1347,6 +1352,7 @@ static int TranslateLayer( OGRDataSource *poSrcDS,
 
             OGRFeature::DestroyFeature( poFeature );
             OGRFeature::DestroyFeature( poDstFeature );
+            VSIFree(panMap);
             return FALSE;
         }
 
@@ -1367,6 +1373,8 @@ end_loop:
 /*      Cleaning                                                        */
 /* -------------------------------------------------------------------- */
     OGRCoordinateTransformation::DestroyCT(poCT);
+    
+    VSIFree(panMap);
 
     return TRUE;
 }

gdal/ogr/ogrfeature.cpp
@@ -2707,6 +2707,7 @@ OGRErr OGRFeature::SetFrom( OGRFeature * poSrcFeature, int bForgiving )
 /*      Retrieve the field ids by name.                                 */
 /* -------------------------------------------------------------------- */
     int         iField, *panMap;
+    OGRErr      eErr;
 
     panMap = (int *) VSIMalloc( sizeof(int) * poSrcFeature->GetFieldCount() );
     for( iField = 0; iField < poSrcFeature->GetFieldCount(); iField++ )
@@ -2719,11 +2720,18 @@ OGRErr OGRFeature::SetFrom( OGRFeature * poSrcFeature, int bForgiving )
             if( bForgiving )
                 continue;
             else
+            {
+                VSIFree(panMap);
                 return OGRERR_FAILURE;
+            }
         }
     }
 
-    return SetFrom( poSrcFeature, panMap, bForgiving );
+    eErr = SetFrom( poSrcFeature, panMap, bForgiving );
+    
+    VSIFree(panMap);
+    
+    return eErr;
 }
 
 /************************************************************************/